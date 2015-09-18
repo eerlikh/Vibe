06b57ea9de4a02dc04d3f09c86f86c9b327c8039
Sam Shull <samuel.c.shull@gmail.com>
2015-09-18 00:55:22 -0400

    added error readout to ruby console in case of validation errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    puts(@user.errors.full_messages)
     redirect_to user_profile_path
   end
 
